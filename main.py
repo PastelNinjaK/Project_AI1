275a656a5d005fe1d4b3e1855f5c3584589ad5cb
Raven Jonric Bernal <bernalraven11@gmail.com>
2026-02-07 20:52:10 -0700

    test

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello World")
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain.chat_models import ChatOpenAI
+from langchain.chat_models import ChatAnthropic 
+
+load_dotenv()
